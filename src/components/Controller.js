aafd4f89afd0555a7345ce1be523ced166f219cf
olliebrownlow <olliebrownlow@gmail.com>
2019-09-29 17:20:13 +0100

    initiates environment and first components

---
+++ src/components/Controller.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Controller extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, this is controller calling!</h1>
+      </div>
+    );
+  }
+}
+
+export default Controller;

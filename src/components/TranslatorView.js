2b4a24e596d4f2ff607003d7fd33bc2c3ca91543
olliebrownlow <olliebrownlow@gmail.com>
2019-09-29 18:12:18 +0100

    wip frontend layout of translator page

--- src/components/TranslatorView.js
+++ src/components/TranslatorView.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 export class TranslatorView extends Component {
 
+  seeAll = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello from translator view!</h1>
-      </div>
+      <React.Fragment>
+      <AppBar
+        style={styles.appBar}
+        position="static"
+      >
+        <Typography variant="h4">
+          English to Pig Latin Translator
+        </Typography>
+      </AppBar>
+      <br/>
+        <Button
+          variant="contained"
+          onClick={this.seeAll}
+          style={styles.button}
+        >
+          See All
+        </Button>
+      </React.Fragment>
     );
+  }
+}
+
+const styles = {
+  button: {
+    backgroundColor: '#ff3399',
+    color: '#ffffff',
+    margin: 15,
+    width: 150
+  },
+  textField: {
+    marginLeft: 15,
+    marginRight: 15,
+    width: 250
+  },
+  appBar: {
+    background: '#ff3399',
+    padding: '20px'
   }
 }
 

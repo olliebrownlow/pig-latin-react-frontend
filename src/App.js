31f87d9b0da02d7c2ede082cbbc261bd9e5e0966
olliebrownlow <olliebrownlow@gmail.com>
2019-12-09 01:19:27 +0000

    removes favicon, changes tab title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import { Controller } from './components/Controller';
-import './App.css';
+import React, { Component } from "react";
+import { Controller } from "./components/Controller";
+import "./App.css";
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">

ae76fa08722be0c46cb9d8ab3405c8f1bd08390d
olliebrownlow <olliebrownlow@gmail.com>
2020-03-28 17:42:35 +0000

    small refactor for ChipButton tests

--- src/__tests__/ChipButton.test.js
+++ src/__tests__/ChipButton.test.js
@@ -4,20 +4,6 @@ import "@testing-library/jest-dom/extend-expect";
 import ChipButton from "../components/SharedComponents/ChipButton";
 
 describe("The ChipButton", () => {
-  // let chipValue;
-  // let functions;
-  // let getByTestId;
-
-  beforeEach(() => {
-    // functions = jest.fn();
-    // chipValue = {
-    //   chipLabel: "Button text",
-    // };
-    // ({ getByTestId } = render(
-    //   <ChipButton functions={functions} chipValue={chipValue} />,
-    // ));
-  });
-
   afterEach(cleanup);
 
   it("shows the right text", () => {
@@ -53,11 +39,10 @@ describe("The ChipButton", () => {
 
     expect(functions.componentDidMount).toHaveBeenCalledTimes(1);
     expect(functions.nextStep).toHaveBeenCalledTimes(1);
-    expect(functions.prevStep).toHaveBeenCalledTimes(0);
+    expect(functions.prevStep).not.toBeCalled;
   });
 
   it("calls the right functions received from the DictionaryView component", () => {
-    // const seeTranslator = jest.fn();
     const functions = {
       componentDidMount: jest.fn(),
       nextStep: jest.fn(),
@@ -74,9 +59,8 @@ describe("The ChipButton", () => {
 
     fireEvent.click(getByTestId("button"));
 
-    expect(functions.componentDidMount).toHaveBeenCalledTimes(0);
-    expect(functions.nextStep).toHaveBeenCalledTimes(0);
+    expect(functions.componentDidMount).not.toBeCalled;
+    expect(functions.nextStep).not.toBeCalled;
     expect(functions.prevStep).toHaveBeenCalledTimes(1);
-    // expect(seeTranslator).toHaveBeenCalledTimes(1);
   });
 });

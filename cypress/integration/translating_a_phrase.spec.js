77dea46404f1ce4e58c05f73427f016bfd03739c
olliebrownlow <olliebrownlow@gmail.com>
2020-05-05 00:12:28 +0100

    adds end to end testing for the translator view

--- cypress/integration/translating_a_phrase.spec.js
+++ cypress/integration/translating_a_phrase.spec.js
@@ -1,14 +1,47 @@
 describe("translating a phrase", () => {
+  const englishPhrase = "risk";
+  const pigLatinPhrase = "iskray";
+  const modalMessage = "Please enter text to translate";
+  const allCharTypeString = "rISk123!?/#";
+
   it("translates the English phrase into Pig Latin and displays it", () => {
-    const englishPhrase = "risk";
-    const pigLatinPhrase = "iskray";
-
     cy.visit("http://localhost:3000");
-
     cy.get('[data-testid="textToTranslate"]').type(englishPhrase);
-
     cy.get('[data-testid="translateButton"]').click();
+    cy.contains(pigLatinPhrase);
+  });
 
+  it("opens a validation error modal when an empty string is submitted for translation, which can be user closed", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('[data-testid="modal"]').should("not.be.visible");
+    cy.get('[data-testid="translateButton"]').click();
+    cy.get('[data-testid="modal"]').should("be.visible");
+    cy.contains(modalMessage);
+    cy.get('[data-testid="modalCloseButton"]').click();
+    cy.get('[data-testid="modal"]').should("not.be.visible");
+  });
+
+  it("opens a validation error modal when an empty string is submitted for translation, which self-closes after 4000ms", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('[data-testid="modal"]').should("not.be.visible");
+    cy.get('[data-testid="translateButton"]').click();
+    cy.get('[data-testid="modal"]').should("be.visible");
+    cy.get('[data-testid="modal"]', { timeout: 4250 }).should("not.be.visible");
+  });
+
+  it("only allows lower case letters to be typed in, accepting capitals as lower case", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('[data-testid="textToTranslate"]').type(allCharTypeString);
+    cy.get('[data-testid="translateButton"]').click();
+    cy.contains(pigLatinPhrase);
+  });
+
+  it("persists the most recently inputted translation", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('[data-testid="textToTranslate"]').type(englishPhrase);
+    cy.get('[data-testid="translateButton"]').click();
+    cy.get('[data-testid="button"]').click();
+    cy.get('[data-testid="button"]').click();
     cy.contains(pigLatinPhrase);
   });
 });
--- src/components/SharedComponents/SnackBarModal.js
+++ src/components/SharedComponents/SnackBarModal.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 
+const TIMEOUT = 4000;
+
 export class SnackBarModal extends Component {
   state = {
     snackbaropen: false,
@@ -14,7 +16,7 @@ export class SnackBarModal extends Component {
     });
   };
 
-  snackbarClose = e => {
+  snackbarClose = (e) => {
     this.setState({
       snackbaropen: false,
     });
@@ -28,12 +30,13 @@ export class SnackBarModal extends Component {
           anchorOrigin={{ vertical: "top", horizontal: "center" }}
           data-testid="modal"
           open={this.state.snackbaropen}
-          autoHideDuration={4000}
+          autoHideDuration={TIMEOUT}
           onClose={this.snackbarClose}
           message={<span>{snackbarmsg.snackbarmsg}</span>}
           action={[
             <IconButton
               key="close"
+              data-testid="modalCloseButton"
               aria-label="close"
               color="inherit"
               onClick={this.snackbarClose}
